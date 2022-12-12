7f950a143cbeaa77a687904926583674498c8a09
AggelosQubit <aggelosqubit@gmail.com>
2022-12-12 11:39:20 +0100

    GSSI ou GISS V0.0.1 Fetch on External Space API OK, Importing Vue component OK

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>Global Solar System Infrastructure</h1>
+	<p> Global solar system infrastructure refers to the idea of building a network of structures and systems in the solar system</p>
+	<p> that would allow humanity to more fully explore and utilize the resources of the solar system. </p>
+	<p> This could include things like space habitats for human settlement, </p>
+	<p> mining operations on asteroids and other celestial bodies, and large-scale solar power stations. </p>
+	<p> The goal of global solar system infrastructure would be to create a sustainable and self-sufficient human presence in the solar system, </p>
+	<p> allowing us to expand our civilization beyond the confines of Earth. </p>
+	<p> This is an ambitious and long-term goal that would require significant technological advances and international cooperation, </p>
+	<p> but it has the potential to greatly benefit humanity and unlock the vast resources of the solar system."</p>
+	
+	<CelestialEntities msg="Test Body" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CelestialEntities from './components/CelestialEntities.vue';
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CelestialEntities
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  /*background-image: url('./assets/01-solar-system-pia12114_orig.webp') ;*/
+  background-image: url('./assets/aba035ac0ef13a0833ca3d3ecc4e592f.jpg') ;
+  font-family: 'Agency FB';
+  background-position: 0 0;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 </style>
---
+++ src/components/CelestialEntities.vue
@@ -0,0 +1,26 @@
+<template>
+    <h1>{{ msg }}</h1>
+	<button @click="fetchCelestialBody()" >Fecth Body</button>
+</template>
+
+<script>
+export default {
+	name: 'CelestialEntities',
+	props: {
+		msg: String
+	},
+	methods:{
+		fetchCelestialBody(){
+			fetch("https://api.le-systeme-solaire.net/rest.php/bodies/mercury")
+			.then( (res)	=> res.json() )
+			.then( (data)	=> console.log(data) )
+		
+		}
+	}
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
